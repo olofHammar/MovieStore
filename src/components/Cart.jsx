1571a93d7fc8067915bb42b965972ed234b9e872
Axl <axel.soderberg1996@gmail.com>
2021-05-31 20:24:01 +0200

    CART TEST

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,17 +5,41 @@ function Cart( { cartItems }) {
     return (
         <div className="cartContainer">
             <div className="cartItems">
-                <h1 className="cartTitle"> Cart Items </h1>
+                <h1 className=""> Cart Items </h1>
                 { cartItems.map(item => (
-                    <div className="cartItem">
-                    <h4>{ item.data.title}</h4>
-                    <h5>{ item.data.quantity }</h5>
+                    <div className="">
+                    <h4>
+                        { item.data.title}
+                        { item.data.price}
+                        <p>
+                        <button onClick={item.data.quantity - 1}> - </button>
+                        {item.data.quantity}
+                        <button onClick={item.data.quantity + 1}> + </button>
+                        </p>
+                        
+                    </h4>
+                    Total price:
+                    
+                    <p>{item.data.price * item.data.quantity} $</p>
+                   
+                    <div>
+                     <img className="movie_poster" src={item.data.poster}/>
+                    </div>
+                    
                     </div>
                 ))
                 }
             </div>
-            <div className="cartTotal">
+            <div className="cartItem">
+            { cartItems.map(item => (
+                <p>{item.data.price} $</p>
+            ))
+            }
                 <h1> Cart Total</h1>
+                
+            </div>
+            <div>
+                <h2><button>Clear Button</button></h2>
             </div>
         </div>
     )
