2860a4dc64a43991434f1f89b2a0e113e3e34aaf
olofHammar <70969820+olofHammar@users.noreply.github.com>
2021-06-03 12:47:32 +0200

    push for this weeks sprint review

--- src/components/navComponents/SidebarData.js
+++ src/components/navComponents/SidebarData.js
@@ -1,11 +1,15 @@
 import React from 'react';
+
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import * as IoIcons from 'react-icons/io';
 import * as RiIcons from 'react-icons/ri';
 
 
+
+
 export const SidebarData  = [
+
   {
     title: 'Home',
     path: '/Home',
@@ -13,66 +17,31 @@ export const SidebarData  = [
     iconClosed: <RiIcons.RiArrowDownSFill />,
     iconOpened: <RiIcons.RiArrowUpSFill />,
   },
-  {
-    title: 'Reports',
-    path: '/reports',
-    icon: <IoIcons.IoIosPaper />,
-    iconClosed: <RiIcons.RiArrowDownSFill />,
-    iconOpened: <RiIcons.RiArrowUpSFill />,
 
-    subNav: [
-      {
-        title: 'Reports',
-        path: '/reports/reports1',
-        icon: <IoIcons.IoIosPaper />,
-        cName: 'sub-nav'
-      },
-      {
-        title: 'Reports 2',
-        path: '/reports/reports2',
-        icon: <IoIcons.IoIosPaper />,
-        cName: 'sub-nav'
-      },
-      {
-        title: 'Reports 3',
-        path: '/reports/reports3',
-        icon: <IoIcons.IoIosPaper />
-      }
-    ]
-  },
-  {
-    title: 'Products',
-    path: '/Cart',
-    icon: <FaIcons.FaCartPlus />
-  },
   {
     title: 'Search Page',
     path: '/SearchPage',
     icon: <IoIcons.IoIosSearch />
   },
-  {
-    title: 'Messages',
-    path: '/messages',
-    icon: <FaIcons.FaEnvelopeOpenText />,
 
+  {
+    title: 'Family',
+    path: '/family',
+    icon: <FaIcons.FaCartPlus />
+  },
+
+  {
+    title: 'MyList',
+    path: '/myList',
+    icon: <FaIcons.FaList />
+  },
+
+  {
+    title: 'Cart',
+    path: '/Cart',
+    icon: <IoIcons.IoIosPaper />,
     iconClosed: <RiIcons.RiArrowDownSFill />,
     iconOpened: <RiIcons.RiArrowUpSFill />,
-    
-
-
-    subNav: [
-      {
-        title: 'Message 1',
-        path: '/messages/message1',
-        icon: <IoIcons.IoIosPaper />
-      },
-      {
-        title: 'Message 2',
-        path: '/messages/message2',
-        icon: <IoIcons.IoIosPaper />
-      }
-    ]
-
   }
 
 ];
